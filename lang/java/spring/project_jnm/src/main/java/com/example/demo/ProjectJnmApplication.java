50522d85e63381ad7609fe4d61bf635114cf4977
fiorenzia <hadesu_1192@yahoo.co.jp>
2023-01-08 20:46:49 +0900

    create spring project

---
+++ lang/java/spring/project_jnm/src/main/java/com/example/demo/ProjectJnmApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectJnmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectJnmApplication.class, args);
+	}
+
+}
